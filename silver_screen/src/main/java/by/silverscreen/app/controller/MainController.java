6a50ea728e28054d0371eb352d5a74a092cdab26
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-18 08:54:34 +0300

    add...

--- silver_screen/src/main/java/by/silverscreen/app/controller/MainController.java
+++ silver_screen/src/main/java/by/silverscreen/app/controller/MainController.java
@@ -4,15 +4,30 @@ import by.silverscreen.app.pojo.Human;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Date;
 
 public class MainController {
 
     private ObservableList<Human> humans = FXCollections.observableArrayList();
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button deleteButton;
 
     @FXML
     private TableView<Human> tableHumans;
@@ -39,6 +54,25 @@ public class MainController {
         birthdayColumn.setCellValueFactory(new PropertyValueFactory<Human, Date>("birthday"));
 
         tableHumans.setItems(humans);
+    }
+
+    @FXML
+    public void addNewHuman() throws IOException {
+        String fxmlFile = "/fxml/add.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
+        Stage stage = new Stage();
+        stage.setTitle("Add new human");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.showAndWait();
+    }
+
+    public ObservableList<Human> getHumans() {
+        return humans;
+    }
+
+    public void setHumans(ObservableList<Human> humans) {
+        this.humans = humans;
     }
 
     private void initData() {
