dc6058ca5b3079b5fdce36c5e74708ccbcc2565e
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-20 11:27:47 +0300

    commit

--- silver_screen/src/main/java/by/silverscreen/app/controller/MainController.java
+++ silver_screen/src/main/java/by/silverscreen/app/controller/MainController.java
@@ -1,69 +1,84 @@
 package by.silverscreen.app.controller;
 
 import by.silverscreen.app.pojo.Human;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-public class MainController {
+public class MainController extends Application {
 
-    private ObservableList<Human> humans = FXCollections.observableArrayList();
+    List<Human> humans = Arrays.<Human>asList(
+            new Human("Alex", 27, "1991-12-20"),
+            new Human("Pavel", 27, "1992-01-01")
+    );
 
     @FXML
     private Button addButton;
-
     @FXML
     private Button editButton;
-
     @FXML
     private Button deleteButton;
-
     @FXML
     private TreeTableView<Human> tableHumans;
-
     @FXML
     private TreeTableColumn<Human, String> nameColumn;
-
     @FXML
     private TreeTableColumn<Human, Integer> ageColumn;
-
     @FXML
     private TreeTableColumn<Human, Date> birthdayColumn;
 
-    @FXML
-    private void initialize() {
+    @Override
+    public void start(Stage stage) throws Exception {
+        TreeItem<Human> root = new TreeItem<>();
 
+        root.setExpanded(true);
+        humans.stream().forEach((human) -> {
+            root.getChildren().add(new TreeItem<>(human));
+        });
+        stage.setTitle("JavaFX and Maven, Tree Table View");
+        stage.setScene(new Scene(
+                new FXMLLoader()
+                        .load(getClass().getResourceAsStream("/fxml/main.fxml"))));
+        Human pavel = new Human("Pavel", 27, "1992-01-01");
+        root.getChildren().add(new TreeItem<>(pavel));
+
+        stage.show();
     }
 
     @FXML
     public void addNewHuman() throws IOException {
-        String fxmlFile = "/fxml/add.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
         Stage stage = new Stage();
         stage.setTitle("Add new human");
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(
+                new FXMLLoader()
+                        .load(getClass().getResourceAsStream("/fxml/add.fxml")))
+        );
         stage.showAndWait();
     }
 
     @FXML
     public void editHuman() throws IOException {
-        String fxmlFile = "/fxml/edit.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
         Stage stage = new Stage();
         stage.setTitle("Edit human");
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(
+                new FXMLLoader()
+                        .load(getClass().getResourceAsStream("/fxml/edit.fxml")))
+        );
         stage.showAndWait();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
     }
 }
