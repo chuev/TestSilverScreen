4ae72f89cd5f2b4b600c134e3175cf10fc19f253
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-21 17:40:16 +0300

    added field validation when adding entity

--- silver_screen/src/main/java/by/silverscreen/app/controller/AddController.java
+++ silver_screen/src/main/java/by/silverscreen/app/controller/AddController.java
@@ -2,11 +2,14 @@ package by.silverscreen.app.controller;
 
 import by.silverscreen.app.pojo.Human;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.sql.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddController {
 
@@ -32,15 +35,41 @@ public class AddController {
     }
 
     public void saveNewHuman() {
-        Human human = new Human(
-                name.getText(),
-                Integer.valueOf(age.getText()),
-                Date.valueOf(birthday.getValue())
-        );
-        addToList(human);
+        if ("".equals(name.getText())) {
+            showWarning("Введите имя Human!");
+        } else if ("".equals(age.getText())) {
+            showWarning("Введите возраст Human!");
+        } else if (!controlAge(age.getText())) {
+            showWarning("Введите корректный возраст Human!");
+        } else if (birthday.getValue() == null) {
+            showWarning("Введите дату рождения Human!");
+        } else if (Date.valueOf(birthday.getValue()).after(new java.util.Date())) {
+            showWarning("Human ещё не родился!");
+        } else {
+            Human human = new Human(
+                    name.getText(),
+                    Integer.valueOf(age.getText()),
+                    Date.valueOf(birthday.getValue())
+            );
+            addToList(human);
+        }
     }
 
     private void addToList(Human human) {
         this.mainController.getHumans().add(human);
+    }
+
+    public void showWarning(String text) {
+        Alert warn = new Alert(Alert.AlertType.ERROR);
+        warn.setTitle("Поле не заполнено!");
+        warn.setHeaderText(text);
+        warn.setContentText("");
+        warn.showAndWait();
+    }
+
+    public boolean controlAge(String string) {
+        Pattern pattern = Pattern.compile("[0-9]{1,3}");
+        Matcher matcher = pattern.matcher(string);
+        return matcher.matches();
     }
 }
